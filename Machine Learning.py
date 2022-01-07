2b25b6e059c2c6ccf27b49614de4458ab4e6aac1
Abith Raj <abith619@gmail.com>
2022-01-07 21:20:36 +0530

    Update Machine Learning.py

--- Machine Learning.py
+++ Machine Learning.py
@@ -265,6 +265,197 @@ Y=f(X)
 # Linear Regression, Logistic Regression, Polynomial Regression, Support Vector Regression,Decision Tree Regression
 # Random Forest Regression, Ridge Regression, Lisso Regression
 
+#                                                                                      Linear Regression
+# makes predictions for continuous/real or numeric variables such as sales, salary, age, product price
+# Simple Linear Regression = to predict the value of a numerical dependent variable
+# Multiple Linear regression = more than one independent variable is used to predict the value of a numerical dependent variable
+#                                  Linear Regression Line
+# Positive Linear Relationship = dependent variable increases on the Y-axis and independent variable increases on X-axis
+# Negative Linear Relationship = dependent variable decreases on the Y-axis and independent variable increases on the X-axis
+#                                   Finding the best fit line
+# the error between predicted values and actual values should be minimized(least error)
+#                                            Cost function
+# Cost function = to find the best fit line, to find the accuracy of the mapping function(Hypothesis function)
+# Mean Squared Error (MSE)  = avg(squared error occurred between the predicted values and actual values)
+#                                                Residuals
+# distance between the actual value and predicted values
+#                                              Gradient Descent
+# minimize the MSE by calculating the gradient of the cost function
+# to update the coefficients of the line by reducing the cost function
+# a random selection of values of coefficient and then iteratively update the values to reach the minimum cost function
+#                                              Model Performance
+# R-squared method = measures the strength of the relationship between the dependent and independent variables on a scale of 0-100%.
+# coefficient of determination, or coefficient of multiple determination for multiple regression
+#                                             Assumptions of Linear Regression
+# Linear relationship = linear relationship between the dependent and independent variables
+# Small or no multicollinearity between the features = high-correlation between the independent variables.
+#                                                        Homoscedasticity Assumption
+# the error term is the same for all the values of independent variables
+#                                                    Normal distribution
+# Normal distribution of error terms = checked using the q-q plot
+# the plot shows a straight line without any deviation, which means the error is normally distributed
+#                                                                    No autocorrelations
+# any correlation in the error term, then it will drastically reduce the accuracy of the model
+# occours when dependency between residual errors
+#                                                                                            Simple Linear Regression
+# dependent variable must be a continuous/real value
+# Forecasting new observations, Model the relationship between the two variables
+#                                                          Simple Linear Regression Model
+y= a0+a1x+ ε"""a0= It is the intercept of the Regression line (can be obtained putting x=0)
+a1= It is the slope of the regression line, which tells whether the line is increasing or decreasing.
+ε = The error term. (For a good model it will be negligible)"""
+# We want to find out if there is any correlation between these two variables
+# We will find the best fit line for the dataset.
+# How the dependent variable is changing by changing the dependent variable.
+#                     Step 1: Data Pre-processing
+import numpy as nm  
+import matplotlib.pyplot as mtp  
+import pandas as pd  
+# Next, we will load the dataset into our code:
+data_set= pd.read_csv('Salary_Data.csv')
+# extract the dependent and independent variables from the given dataset
+x= data_set.iloc[:, :-1].values
+y= data_set.iloc[:, 1].values
+# split both variables into the test set and training set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 1/3, random_state=0)
+#                                        Fitting the Simple Linear Regression to the Training Set
+# overfitting can produce misleading R-squared values, regression coefficients, and p-values
+# import the LinearRegression class of the linear_model library from the scikit learn, regressor = object of the class
+#                                      Fitting the Simple Linear Regression model to the training dataset  
+from sklearn.linear_model import LinearRegression
+regressor= LinearRegression()
+regressor.fit(x_train, y_train) # fit() method to fit our Simple Linear Regression object to the training set
+# o/p = LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False)
+#                                     Prediction of test, training-set result(Training)
+# Visualizing the Training set results, performance of our model 
+mtp.scatter(x_train, y_train, color="green")   # create a scatter plot of observations.
+mtp.plot(x_train, x_pred, color="red")    # plot the regression line, predicted salary for training set x_pred, and color of the line
+mtp.title("Salary vs Experience (Training Dataset)")  # title for the plot
+mtp.xlabel("Years of Experience")  # assign labels for x-axis and y-axis
+mtp.ylabel("Salary(In Rupees)")  
+mtp.show()
+#                                          visualizing the Test set results(Test)
+mtp.scatter(x_test, y_test, color="blue")   # Test
+mtp.plot(x_train, x_pred, color="red")      # Train
+mtp.title("Salary vs Experience (Test Dataset)")  
+mtp.xlabel("Years of Experience")  
+mtp.ylabel("Salary(In Rupees)")  
+mtp.show()
+#                                                                                                    Multiple Linear Regression
+# response variable is affected by more than one predictor variable
+# (MLR) is to model the linear relationship between the explanatory (independent) variables and response (dependent) variable
+# linear relationship between a single dependent continuous variable and more than one independent variable
+# For MLR, the dependent or target variable(Y) must be the continuous/real
+# predictor or independent variable may be of continuous or categorical form
+Y= b0+b1x1+ b2x2+ b3x3+...... bnxn       ............... (a)
+"""Y= Output/Response variable
+b0, b1, b2, b3 , bn....= Coefficients of the model.
+x1, x2, x3, x4,...= Various Independent/feature variable"""
+#                Assumptions for Multiple Linear Regression
+# Multivariate Normality = residuals are normally distributed.
+# No Multicollinearity = independent variables are not highly correlated with each other, tested using Variance Inflation Factor (VIF) values
+# Homoscedasticity = variance of error terms are similar across the values of the independent variables
+# at least two independent variables, which can be nominal, ordinal, or interval/ratio level variables.
+# The regression residuals must be normally distributed, MLR assumes little or no multicollinearity in data
+#                                                             implementation of Multiple Linear Regression model
+#                                                 Problem
+"""We have a dataset of 50 start-up companies. This dataset contains five main information: 
+R&D Spend, Administration Spend, Marketing Spend, State, and Profit for a financial year. Our goal is to create a model that can 
+easily determine which company has a maximum profit, and which is the most affecting factor for the profit of a company."""
+#                                Data Pre-processing Steps
+import numpy as nm  
+import matplotlib.pyplot as mtp  
+import pandas as pd
+#                   importing datasets  
+data_set= pd.read_csv('50_CompList.csv')
+#                   Extracting Independent and dependent Variable  
+x= data_set.iloc[:, :-1].values
+y= data_set.iloc[:, 4].values
+#                                         Encoding Dummy Variables
+#                      Catgorical data  
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder  
+labelencoder_x= LabelEncoder()  
+x[:, 3]= labelencoder_x.fit_transform(x[:,3])  
+onehotencoder= OneHotEncoder(categorical_features= [3])    #  create the dummy variables
+x= onehotencoder.fit_transform(x).toarray()
+#                               avoiding the dummy variable trap
+x = x[:, 1:]
+#                           Splitting the dataset into training and test set.  
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.2, random_state=0)
+
+# Fitting the MLR model to the training set
+from sklearn.linear_model import LinearRegression  
+regressor= LinearRegression()  
+regressor.fit(x_train, y_train)
+# o/p = LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False)
+# Predicting the result of the test set
+y_pred= regressor.predict(x_test) #               create a y_pred vector
+#                                 check the score for training dataset and test dataset
+print('Train Score: ', regressor.score(x_train, y_train))
+print('Test Score: ', regressor.score(x_test, y_test))
+"""Effectiveness of Independent variable on prediction.
+Predicting the impact of changes
+to find the relations between two or more variables""" 
+#                                                                                   Terminologies Related to the Regression Analysis
+# Dependent Variable = a target variable to predict or understand is called the dependent variable
+# Independent Variable (predictor) = used to predict the values of the dependent variables
+# Multicollinearity = variables are highly correlated with eachother
+# Outliers = points on the graph that fall significantly outside the cloud made up of other points
+# Overfitting = occurrence when the variables start to show random errors rather than efficiently describing the relationship among the variables
+# Underfitting = number of variables scarcely fits a given model
+# Heteroscedasticity (heteroskedasticity) = while reading variable’s standard error (SE) measured over a given time is not constant
+#                                                                                         $         Linear Regression
+# Analyzing trends, sales estimates, Salary forecasting, Real estate prediction, Arriving at ETAs in traffic.
+#                                                                                         $     Logistic regression(sigmoid function)
+# categorical variable such as 0 or 1, Yes or No, True or False, Spam or not spam
+# sigmoid function is used to model the data, sig(x)=1/1+e^-x
+# Binary(0/1, pass/fail), Multi(cats, dogs, lions), Ordinal(low, medium, high)
+#                                                                                         $   Support Vector Machine
+# Kernel : is a function used to map a lower-dimensional data into higher dimensional data
+# Hyperplane: SVM is a separation line between two classes
+# in SVR, line which helps to predict the continuous variables and cover most of the datapoints
+# Boundary lines are the two lines apart from hyperplane, which creates a margin for datapoints
+# Support vectors are the datapoints which are nearest to the hyperplane and opposite class
+#                                                                                             $      Decision Tree Regression
+# solve problems for both categorical and numerical data
+# tree-like structure in which each internal node represents the "test" for an attribute, 
+# each branch represent the result of the test, and each leaf node represents the final decision or result
+# is constructed starting from the root node/parent node (dataset), which splits into left and right child nodes (subsets of dataset).
+# These child nodes are further divided into their children node, and themselves become the parent node of those nodes
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+pd.set_option('display.max_columns',None)
+df = pd.read_csv("train.csv")
+df.head()
+df.drop(['PassengerId','SibSp','Name','Parch','Fare','Ticket','Cabin','Embarked'],axis='columns',inplace=True)
+df.head()
+inputs = df.drop('Survived',axis='columns')
+target = df.Survived
+inputs.Sex = inputs.Sex.map({'male': 1, 'female': 2})
+inputs.head()
+inputs.Age[:10]
+inputs.Age = inputs.Age.fillna(inputs.Age.mean())
+inputs.Age[:10]
+X_train, X_test, y_train, y_test = train_test_split(inputs,target,test_size=0.2)
+model = tree.DecisionTreeClassifier()
+model.fit(X_train,y_train)
+model.score(X_test,y_test)
+#                                                                                           $      Random Forest Regression
+# ensemble learning method which combines multiple decision trees and predicts the final output based on the average of each tree output
+# g(x)= f0(x)+ f1(x)+ f2(x)+.,,,
+# Bagging or Bootstrap Aggregation technique of ensemble learning in which aggregated decision tree runs in parallel and do not interact with each other
+# Overfitting in the model by creating random subsets of the dataset
+#                                                                                            $       Ridge Regression
+# The amount of bias added to the model is known as Ridge Regression penalty
+# by multiplying with the lambda to the squared weight of each individual features 
+βridge=(X′X+λI)−1(X′Y)
+# high collinearity between the independent variables, regularization technique, which is used to reduce the complexity of the model
+#                                                                                               $       Lasso Regression
+# penalty term contains only the absolute weights instead of a square of weights
+# it takes absolute values, hence, it can shrink the slope to 0
 
 #                                                                                                              Data preprocessing
 # Reader, 
