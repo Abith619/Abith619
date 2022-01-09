42b5f71889f3bc4da8f93f9e793d95a8d2b51c89
Abith Raj <abith619@gmail.com>
2022-01-09 19:28:37 +0530

    Update Machine Learning.py

--- Machine Learning.py
+++ Machine Learning.py
@@ -176,6 +176,97 @@ y=f(x)  # y = categorical output
 #                                                                            Naïve Bayes
 # an extension of the Bayes theorem wherein each feature assumes independence
 # easy and quick way to predict the class of the dataset, multi-class prediction
+#                                                                           Naive Bayes Classifier Algorithm
+# text classification that includes a high-dimensional training dataset
+# models that assign class labels to problem instances, represented as vectors of feature values
+#                                                                   Bayes' Theorem:
+"""P(c|x) is Posterior probability: Probability of hypothesis A on the observed event B.
+P(x|c) is Likelihood probability: Probability of the evidence given that the probability of a hypothesis is true.
+P(c) is Class Prior Probability: Probability of hypothesis before observing the evidence.
+P(x) is Predictor Prior Probability: Probability of Evidence."""
+#                                                                         Working of Naïve Bayes' Classifier:
+"""Convert the given dataset into frequency tables.
+Generate Likelihood table by finding the probabilities of given features.
+Now, use Bayes theorem to calculate the posterior probability"""
+#                                                                     Applying Bayes'theorem:
+P(Yes|Sunny)= P(Sunny|Yes)*P(Yes)/P(Sunny)
+P(Sunny|Yes)= 3/10= 0.3
+P(Sunny)= 0.35
+P(Yes)=0.71
+So P(Yes|Sunny) = 0.3*0.71/0.35= 0.60
+
+P(Yes|Sunny)>P(No|Sunny)
+P(No|Sunny)= P(Sunny|No)*P(No)/P(Sunny)
+P(Sunny|NO)= 2/4=0.5
+P(No)= 0.29
+P(Sunny)= 0.35
+So P(No|Sunny)= 0.5*0.29/0.35 = 0.41
+#                                        Applications
+# Multi-class Prediction, Recommendation System,  real-time predictions, Text classification such as Spam filtering and Sentiment analysis
+#                                                              3 Types = Gaussian, Multinomial, Bernoulli
+#                                                              Implementation of the Naïve Bayes algorithm
+"""Data Pre-processing step
+Fitting Naive Bayes to the Training set
+Predicting the test result
+Test accuracy of the result(Creation of Confusion matrix)
+Visualizing the test set result."""
+#                                     Importing the dataset  
+dataset = pd.read_csv('user_data.csv')
+x = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+#                                           Splitting the dataset into the Training set and Test set  
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
+#                                        Feature Scaling  
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+#                                  Fitting Naive Bayes to the Training set  
+from sklearn.naive_bayes import GaussianNB  
+classifier = GaussianNB()  
+classifier.fit(x_train, y_train)
+#                                         Predicting the Test set results  
+y_pred = classifier.predict(x_test)
+#                                               Making the Confusion Matrix  
+from sklearn.metrics import confusion_matrix  
+cm = confusion_matrix(y_test, y_pred)
+#                                                        Visualising the Training set results  
+from matplotlib.colors import ListedColormap  
+x_set, y_set = x_train, y_train  
+X1, X2 = nm.meshgrid(nm.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),  
+                     nm.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))  
+mtp.contourf(X1, X2, classifier.predict(nm.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),  
+             alpha = 0.75, cmap = ListedColormap(('purple', 'green')))  
+mtp.xlim(X1.min(), X1.max())  
+mtp.ylim(X2.min(), X2.max())  
+for i, j in enumerate(nm.unique(y_set)):  
+    mtp.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],  
+                c = ListedColormap(('purple', 'green'))(i), label = j)  
+mtp.title('Naive Bayes (Training set)')  
+mtp.xlabel('Age')  
+mtp.ylabel('Estimated Salary')  
+mtp.legend()  
+mtp.show()
+#                                                  Visualising the Test set results  
+from matplotlib.colors import ListedColormap  
+x_set, y_set = x_test, y_test  
+X1, X2 = nm.meshgrid(nm.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),  
+                     nm.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))  
+mtp.contourf(X1, X2, classifier.predict(nm.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),  
+             alpha = 0.75, cmap = ListedColormap(('purple', 'green')))  
+mtp.xlim(X1.min(), X1.max())  
+mtp.ylim(X2.min(), X2.max())  
+for i, j in enumerate(nm.unique(y_set)):  
+    mtp.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],  
+                c = ListedColormap(('purple', 'green'))(i), label = j)  
+mtp.title('Naive Bayes (test set)')  
+mtp.xlabel('Age')  
+mtp.ylabel('Estimated Salary')  
+mtp.legend()  
+mtp.show()
+
+
 #                                                                                                 Decision Tree Classification
 # used for both predictions & classifications, 2Types = Induction & Pruning
 # map the various outcomes that are a result of the consequences or decisions
@@ -681,6 +772,21 @@ mtp.show()
 # g(x)= f0(x)+ f1(x)+ f2(x)+.,,,
 # Bagging or Bootstrap Aggregation technique of ensemble learning in which aggregated decision tree runs in parallel and do not interact with each other
 # Overfitting in the model by creating random subsets of the dataset
+#                                                                    Random forest creation pseudocode
+# Pseudocode to perform prediction from the created random forest classifier
+"""Randomly select “k” features from total “m” features.
+Where k << m
+Among the “k” features, calculate the node “d” using the best split point.
+Split the node into daughter nodes using the best split.
+Repeat 1 to 3 steps until “l” number of nodes has been reached.
+Build forest by repeating steps 1 to 4 for “n” number times to create “n” number of trees."""
+#                                                  Random forest prediction pseudocode
+"""Takes the test features and use the rules of each randomly created decision tree to predict the oucome and stores the predicted outcome (target)
+Calculate the votes for each predicted target.
+Consider the high voted predicted target as the final prediction from the random forest algorithm."""
+
+
+
 #                                                                                            $       Ridge Regression
 # The amount of bias added to the model is known as Ridge Regression penalty
 # by multiplying with the lambda to the squared weight of each individual features 
