fb14434cd6174c982b2cffda1411c3c8e715e141
Abith Raj <abith619@gmail.com>
2022-01-31 21:38:48 +0530

    Medical Store Management

--- Project.py
+++ Project.py
@@ -658,4 +658,81 @@ treeview.column(“#5" ,stretch=NO,width=70,minwidth=0)
 treeview.column(“#6" ,stretch=NO,width=70,minwidth=0)
 treeview.column(“#7" ,stretch=NO,width=70,minwidth=0)
 treeview.pack()
-#########################Lets connect the database and fetch values #########
+#########################Lets connect the database and fetch values ########
+#                                                                             REVENUE Details
+def show_rev(): # opens revenue window-----------------------------------------------------------------------TOTAL REVENUE
+    global c, cur, flag,rev
+    apt.destroy()
+    cb=('cus_name','cus_add','items','Total_cost','bill_dt','bill_no','bill','val_id')
+    flag='rev'
+    rev=Tk()
+    total=0.0
+    today=str(time.localtime()&#91;2])+'/'+str(time.localtime()&#91;1])+'/'+str(time.localtime()&#91;0])
+    Label(rev,text='Today: '+today).grid(row=0,column=0)
+    cur.execute('select * from bills')
+    for i in cur:
+        if i&#91;4]==today:
+            total+=float(i&#91;3])
+    print (total)
+    Label(rev,width=22,text='Total revenue: PHP '+str(total), bg='blue',fg='white').grid(row=1,column=0)
+    cx=0
+    vsb=Scrollbar(orient='vertical')
+    lb1=Listbox(rev,width=25, yscrollcommand=vsb.set)
+    vsb.grid(row=2,column=1,sticky=N+S)
+    lb1.grid(row=2,column=0)
+    vsb.config( command = lb1.yview )
+    cur.execute("select * from bills")
+    for i in cur:
+        if i&#91;4]==today:
+            cx+=1
+            lb1.insert(cx,'Bill No.: '+str(i&#91;5])+'    : PHP '+str(i&#91;3]))
+    c.commit()
+    Button(rev,text='Main Menu',bg='green', fg='white',command=main_menu).grid(row=15,column=0)
+    rev.mainloop()
+#                                                   BILLING system
+def billing(): # to create bills for customer-------------------------------------------------------------BILLING system
+    global c, cur, apt, flag, t, name, name1, add, st, names, qty, sl, qtys, vc_id, n, namee, lb1
+    t=0
+    vc_id=''
+    names=&#91;]
+    qty=&#91;]
+    sl=&#91;]
+    n=&#91;]
+    qtys=&#91;'']*10
+    cur.execute("select *from med")
+    for i in cur:
+        n.append(i&#91;1])
+    c.commit()
+    if flag=='st':
+        st.destroy()
+    else:
+        apt.destroy()
+    flag='st'
+    st=Tk()
+    st.title('BILLING SYSTEM')
+    Label(st,text='-'*48+'BILLING SYSTEM'+'-'*49).grid(row=0,column=0,columnspan=7)
+    Label(st,text='Enter Name: ').grid(row=1,column=0)
+    name1=Entry(st)
+    name1.grid(row=1, column=1)
+    Label(st,text='Enter Address: ').grid(row=2,column=0)
+    add=Entry(st)
+    add.grid(row=2, column=1)
+    Label(st,text="Value Id (if available)").grid(row=3, column=0)
+    vc_id=Entry(st)
+    vc_id.grid(row=3, column=1)
+    Button(st,text='Check V.C.', bg='green', fg='white', command=blue).grid(row=4, column=0)
+    Label(st,text='-'*115).grid(row=6, column=0,columnspan=7)
+    Label(st,text='SELECT PRODUCT',width=25,relief='ridge').grid(row=7, column=0)
+    Label(st,text=' RACK  QTY LEFT     COST          ',width=25,relief='ridge').grid(row=7, column=1)
+    Button(st,text='Add to bill', bg='blue', fg='white', width=15,command=append2bill).grid(row=8, column=6)
+    Label(st,text='QUANTITY',width=20,relief='ridge').grid(row=7, column=5)
+    qtys=Entry(st)
+    qtys.grid(row=8,column=5)
+    refresh()
+    Button(st,width=15,text='Main Menu', bg='green', fg='white', command=main_menu).grid(row=1,column=6)
+    Button(st,width=15,text='Refresh Stock',bg='skyblue', fg='black', command=refresh).grid(row=3,column=6)
+    Button(st,width=15,text='Reset Bill', bg='red', fg='white', command=billing).grid(row=4,column=6)
+    Button(st,width=15,text='Print Bill', bg='orange', fg='white', command=print_bill).grid(row=5,column=6)
+    Button(st,width=15,text='Save Bill', bg='blue', fg='white', command=make_bill).grid(row=7,column=6)
+    
+    st.mainloop()
