3361d7918f6034c823366a2da9e5f280a6a3a0e2
Abith Raj <89273707+Abith619@users.noreply.github.com>
2022-03-05 18:10:21 +0530

    Add files via upload

---
+++ Pattern Programs/19.py
@@ -0,0 +1,6 @@
+for i in range(5):
+    for j in range(5, 0, -1):
+        if i==j:
+            print("*")
+        else:
+            print(j)
---
+++ Pattern Programs/6.py
@@ -0,0 +1,3 @@
+n=5
+for i in range(n+1):
+    print((n-i)*" "+"*"*i)
